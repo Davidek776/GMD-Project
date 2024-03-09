1cc111b2486438525204f6241f9fd01257b74aa4
David Kocur <62095094+Davidek776@users.noreply.github.com>
2024-03-09 15:56:14 +0100

    Work on pause menu.

---
+++ Lost Together/Assets/PauseMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
