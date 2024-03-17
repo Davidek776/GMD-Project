12bc58d375717dba412ead532f06ae52d37b2449
Simon Mayer <89986425+simonfrayer@users.noreply.github.com>
2024-03-17 22:17:57 +0100

    Initialization and start of development on Lost Together (#6)
    * added unity gitignore
    
    * project init
    
    * project setup
    
    * added main menu
    
    * Add animation effect to buttons.
    
    * Add animation to back button.
    
    * Work on pause menu.
    
    * Add pause menu.
    
    * Input system + Splitscreen (#3)
    
    * Input system + Splitscreen
    
    * Updated Player 2 with player2 movement script
    
    * Added heavy object + create script for heavy object + disabled collision between players (#4)
    
    ---------
    
    Co-authored-by: David Kocur <62095094+Davidek776@users.noreply.github.com>

---
+++ Lost Together/Assets/Scripts/Pause Menu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+
+    [SerializeField]
+    private GameObject pauseMenu;
+
+
+    void Start(){
+        pauseMenu.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isGamePaused){
+                ResumeGame();
+            }
+            else{
+                PauseGame();
+            }
+        }
+    }
+    public void ResumeGame(){
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+     void PauseGame(){
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void LoadMenu(){
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
