dbb6dacaf8ffda0ef2a7e780cde27591d34ca8fd
Simon Mayer <89986425+simonfrayer@users.noreply.github.com>
2024-04-07 17:16:03 +0200

    Feature/respawn (#10)
    * added respawn controller
    
    * added respawn and dying animation, ass well as prefab spikes that kill the player when hit (trigger)
    
    * improved dying/respawning animation

---
+++ Lost Together/Assets/Scripts/Respawn.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    private Vector2 startPos;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("DeadZone"))
+        {
+            Die(0f);
+        }
+        else if (collision.CompareTag("Spike"))
+        {
+            animator.SetBool("isDead", true);
+            Die(0.5f);
+        }
+    }
+
+    private void Die(float respawnDuration)
+    {
+        Debug.Log("Is dead: " + animator.GetBool("isDead"));
+        StartCoroutine(RespawnPlayer(respawnDuration));
+    }
+
+    IEnumerator RespawnPlayer(float duration = 0)
+    {
+        yield return new WaitForSeconds(duration);
+        animator.SetBool("isDead", false);
+        Debug.Log("Is dead: " + animator.GetBool("isDead"));
+        transform.position = startPos;
+    }
+}
