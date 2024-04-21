a583931e331d9475524a8bd3a2f59531cde69c49
Simon <simon.mayer@lego.com>
2024-04-21 11:31:52 +0200

    inproved main menu + options menu to be nevigatable by controller + butoon animations fixed for selecting

--- Lost Together/Assets/Scripts/Main Menu.cs
+++ Lost Together/Assets/Scripts/Main Menu.cs
@@ -1,13 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu, optionsMenu;
+    public GameObject mainFirstButton, optionsFirstButton, optionsCloseButton;
+
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(mainFirstButton); 
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OpenOptionsMenu()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+    }
+
+    public void CloseOptionsMenu()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsCloseButton);
     }
 
     public void QuitGame()
