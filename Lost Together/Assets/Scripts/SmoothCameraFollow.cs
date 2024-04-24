3f51ee645d7e3df726e147531a4b4098d4e75510
Simon Mayer <89986425+simonfrayer@users.noreply.github.com>
2024-04-24 12:52:25 +0200

    fixed camera movement to be ahead of the player in the direction of m… (#19)
    * fixed camera movement to be ahead of the player in the direction of movement and used the Lerp function to smoothen stings out
    
    * fixed target assigning and deadZone

--- Lost Together/Assets/Scripts/SmoothCameraFollow.cs
+++ Lost Together/Assets/Scripts/SmoothCameraFollow.cs
@@ -4,18 +4,64 @@ using UnityEngine;
 
 public class SmoothCameraFollow : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0, 2f, -10);
     private Vector3 velocity = Vector3.zero;
+    private MovementController movementController;
+    private Vector3 targetOffset;
+    private Vector3 currentOffset;
 
-    public Transform target;
+    public GameObject target;
     public float smoothSpeed = 0.25f;
+    public float directOffsetX = 0f;  
+    public float aheadOffsetX = 5f; 
+    public float sideOffsetX = 5f;
+    public float offsetChangeSpeed = 5f;
 
-    // Update is called once per frame
+    void Start()
+    {
+        movementController = target.GetComponent<MovementController>();
+        currentOffset = targetOffset = CalculateOffset();
+    }
+
     void Update()
     {
-        Vector3 targetPosition = target.position + offset;
+        float horizontalInput = movementController.GetHorizontalInput();
+
+        if (horizontalInput == 0)
+        {
+            targetOffset = new Vector3(directOffsetX, 2f, -10);
+        }
+        else if (horizontalInput > 0)
+        {
+            targetOffset = new Vector3(aheadOffsetX, 2f, -10);
+        }
+        else
+        {
+            targetOffset = new Vector3(-aheadOffsetX, 2f, -10);
+        }
+
+        currentOffset = Vector3.Lerp(currentOffset, targetOffset, smoothSpeed * Time.deltaTime * offsetChangeSpeed);
+
+        Vector3 targetPosition = target.transform.position + currentOffset;
         Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothSpeed);
 
         transform.position = smoothedPosition;
+    }
+
+    Vector3 CalculateOffset()
+    {
+        float horizontalInput = movementController.GetHorizontalInput();
+
+        if (horizontalInput == 0)
+        {
+            return new Vector3(directOffsetX, 2f, -10);
+        }
+        else if (horizontalInput > 0)
+        {
+            return new Vector3(aheadOffsetX, 2f, -10);
+        }
+        else
+        {
+            return new Vector3(-aheadOffsetX, 2f, -10);
+        }
     }
 }
