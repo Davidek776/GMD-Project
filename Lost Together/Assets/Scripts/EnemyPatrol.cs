fb16ed5f02736ae0907eda8f3142f492374ba310
David Kocur <62095094+Davidek776@users.noreply.github.com>
2024-05-06 11:35:24 +0200

    Create basic patrolling.

--- Lost Together/Assets/Scripts/EnemyPatrol.cs
+++ Lost Together/Assets/Scripts/EnemyPatrol.cs
@@ -7,13 +7,15 @@ public class EnemyPatrol : MonoBehaviour
     public GameObject pointA;
     public GameObject pointB;
 
-    private RigidBody2D rb;
+    private Rigidbody2D rb;
     private Animator anim;
     private Transform currentPoint;
 
+    public float speed;
+
     void Start()
     {
-    rb=GetComponent<RigidBody2D>();
+    rb=GetComponent<Rigidbody2D>();
     anim=GetComponent<Animator>();
     currentPoint=pointB.transform;
     anim.SetBool("isRunning", true);
@@ -22,5 +24,34 @@ public class EnemyPatrol : MonoBehaviour
     void Update()
     {
         Vector2 point=currentPoint.position-transform.position;
+        if(currentPoint==pointB.transform){
+            rb.velocity=new Vector2(speed,0);
+        }
+        else{
+            rb.velocity=new Vector2(-speed,0);
+        }
+
+        if(Vector2.Distance(transform.position,currentPoint.position)<0.5f && currentPoint==pointB.transform){
+            Flip();
+            currentPoint=pointA.transform;
+        }
+
+        if(Vector2.Distance(transform.position,currentPoint.position)<0.5f && currentPoint==pointA.transform){
+            Flip();
+            currentPoint=pointB.transform;
+
+        }
+    }
+
+    private void OnDrawGizmos(){
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    }
+
+    private void Flip(){
+        Vector3 localScale=transform.localScale; 
+        localScale.x*=-1;
+        transform.localScale=localScale;
     }
 }
