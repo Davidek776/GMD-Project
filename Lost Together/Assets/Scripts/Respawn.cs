0a5d82f6ca299bc498447696fcd2f0a3d0b05f2a
David Kocur <62095094+Davidek776@users.noreply.github.com>
2024-05-10 11:02:06 +0200

    Update respawn script.

--- Lost Together/Assets/Scripts/Respawn.cs
+++ Lost Together/Assets/Scripts/Respawn.cs
@@ -17,24 +17,15 @@ public class Respawn : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-            Debug.Log("HERE"+collision.name);
-
         if (collision.CompareTag("DeadZone"))
         {
             Die(0f);
-        }
-
-          if (collision.CompareTag("Enemy"))
-        {
-        Die(0.5f);
-        Debug.Log("Die");
         }
 
         else if (collision.CompareTag("Spike") || collision.CompareTag("Enemy"))
         {
             animator.SetBool("isDead", true);
             Die(0.5f);
-            Debug.Log("Die");
         }
     }
     
