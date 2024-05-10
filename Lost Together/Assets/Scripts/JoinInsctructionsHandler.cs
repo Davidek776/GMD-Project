839e38b853c8c3a706b7b1d46f280a396d472b6a
Simon <simon.mayer@lego.com>
2024-05-10 14:25:27 +0200

    refactor

--- Lost Together/Assets/Scripts/JoinInsctructionsHandler.cs
+++ Lost Together/Assets/Scripts/JoinInsctructionsHandler.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 public class JoinInsctructionsHandler : MonoBehaviour
 {
