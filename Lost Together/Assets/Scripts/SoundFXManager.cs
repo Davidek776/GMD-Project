1b424c06457ac1abbe591accd1e242786cf699b7
Simon <simon.mayer@lego.com>
2024-05-28 21:22:46 +0200

    added deatch sound effect

--- Lost Together/Assets/Scripts/SoundFXManager.cs
+++ Lost Together/Assets/Scripts/SoundFXManager.cs
@@ -13,6 +13,7 @@ public class SoundFXManager : MonoBehaviour
 
     [Header("Volumes")]
     [Range(0, 1)][SerializeField] private float jumpVolume = 0.75f;
+    [Range(0, 1)][SerializeField] private float deathVolume = 0.75f;
     [Range(0, 1)][SerializeField] private float runningVolume = 1f;
     [Range(0, 1)][SerializeField] private float buttonVolume = 0.25f;
 
@@ -20,6 +21,8 @@ public class SoundFXManager : MonoBehaviour
     private AudioSource jumpAudioSourcePlayer2;
     private AudioSource runningAudioSourcePlayer1;
     private AudioSource runningAudioSourcePlayer2;
+    private AudioSource deathAudioSourcePlayer1;
+    private AudioSource deathAudioSourcePlayer2;
     private AudioSource buttonAudioSource;
 
     private void Awake()
@@ -42,40 +45,40 @@ public class SoundFXManager : MonoBehaviour
         Destroy(audioSource.gameObject, clipLength);
     }
 
-    public void PlayJumpSound(AudioClip clip, Transform spawnTransform, int playerIndex)
+    public void PlayRandomJumpSound(AudioClip[] clip, Transform spawnTransform, int playerIndex)
     {
         if (playerIndex == 0)
-            PlayJumpSoundPlayer1(clip, spawnTransform, jumpVolume);
+            PlayRandomJumpSoundPlayer1(clip, spawnTransform, jumpVolume);
         else
-            PlayJumpSoundPlayer2(clip, spawnTransform, jumpVolume);
+            PlayRandomJumpSoundPlayer2(clip, spawnTransform, jumpVolume);
     }
 
-    private void PlayJumpSoundPlayer1(AudioClip clip, Transform spawnTransform, float volume)
+    private void PlayRandomJumpSoundPlayer1(AudioClip[] clip, Transform spawnTransform, float volume)
     {
         if (jumpAudioSourcePlayer1 != null)
             return;
 
         jumpAudioSourcePlayer1 = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
 
-        jumpAudioSourcePlayer1.clip = clip;
+        jumpAudioSourcePlayer1.clip = clip[Random.Range(0, clip.Length)];
         jumpAudioSourcePlayer1.volume = volume;
         jumpAudioSourcePlayer1.Play();
-        float clipLength = clip.length;
+        float clipLength = jumpAudioSourcePlayer1.clip.length;
 
         Destroy(jumpAudioSourcePlayer1.gameObject, clipLength);
     }
 
-    private void PlayJumpSoundPlayer2(AudioClip clip, Transform spawnTransform, float volume)
+    private void PlayRandomJumpSoundPlayer2(AudioClip[] clip, Transform spawnTransform, float volume)
     {
         if (jumpAudioSourcePlayer2 != null)
             return;
 
         jumpAudioSourcePlayer2 = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
 
-        jumpAudioSourcePlayer2.clip = clip;
+        jumpAudioSourcePlayer2.clip = clip[Random.Range(0, clip.Length)];
         jumpAudioSourcePlayer2.volume = volume;
         jumpAudioSourcePlayer2.Play();
-        float clipLength = clip.length;
+        float clipLength = jumpAudioSourcePlayer2.clip.length;
 
         Destroy(jumpAudioSourcePlayer2.gameObject, clipLength);
     }
@@ -116,6 +119,44 @@ public class SoundFXManager : MonoBehaviour
         float clipLength = runningAudioSourcePlayer2.clip.length;
 
         Destroy(runningAudioSourcePlayer2.gameObject, clipLength);
+    }
+
+    public void PlayRandomDeathSound(AudioClip[] clip, Transform spawnTransform, int playerIndex)
+    {
+        if (playerIndex == 0)
+            PlayRandomDeathSoundPlayer1(clip, spawnTransform, deathVolume);
+        else
+            PlayRandomDeathSoundPlayer2(clip, spawnTransform, deathVolume);
+    }
+
+    private void PlayRandomDeathSoundPlayer1(AudioClip[] clip, Transform spawnTransform, float volume)
+    {
+        if (deathAudioSourcePlayer1 != null)
+            return;
+
+        deathAudioSourcePlayer1 = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        deathAudioSourcePlayer1.clip = clip[Random.Range(0, clip.Length)];
+        deathAudioSourcePlayer1.volume = volume;
+        deathAudioSourcePlayer1.Play();
+        float clipLength = deathAudioSourcePlayer1.clip.length;
+
+        Destroy(deathAudioSourcePlayer1.gameObject, clipLength);
+    }
+
+    private void PlayRandomDeathSoundPlayer2(AudioClip[] clip, Transform spawnTransform, float volume)
+    {
+        if (deathAudioSourcePlayer2 != null)
+            return;
+
+        deathAudioSourcePlayer2 = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        deathAudioSourcePlayer2.clip = clip[Random.Range(0, clip.Length)];
+        deathAudioSourcePlayer2.volume = volume;
+        deathAudioSourcePlayer2.Play();
+        float clipLength = deathAudioSourcePlayer2.clip.length;
+
+        Destroy(deathAudioSourcePlayer2.gameObject, clipLength);
     }
 
 
