a1de85c6fc5ea20525785d54db87d3b78b35aeac
Simon <simon.mayer@lego.com>
2024-06-04 18:56:58 +0200

    add next level

--- Lost Together/Assets/Scripts/GameOverMenu.cs
+++ Lost Together/Assets/Scripts/GameOverMenu.cs
@@ -9,30 +9,45 @@ public class GameOverMenu : MonoBehaviour
     public GameObject gameOverFirstButton;
 
     public bool isActive;
-     void Start()
+    void Start()
     {
         isActive = false;
-        gameObject.SetActive(false);        
+        gameObject.SetActive(false);
     }
 
     public void Show()
     {
         gameObject.SetActive(true);
         EventSystem.current.SetSelectedGameObject(null);
-        EventSystem.current.SetSelectedGameObject(gameOverFirstButton); 
+        EventSystem.current.SetSelectedGameObject(gameOverFirstButton);
         Time.timeScale = 0f;
-        isActive=true;
+        isActive = true;
     }
 
-     public void RestartGame()
+    public void RestartGame()
     {
         SceneLoader.instance.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1f;
-        }
+    }
 
-     public void LoadMenu()
+    public void LoadMenu()
     {
         SceneLoader.instance.LoadScene("Menu");
+        Time.timeScale = 1f;
+    }
+
+    public void NextLevel()
+    {
+        int mexScene = SceneManager.sceneCountInBuildSettings;
+        if (SceneManager.GetActiveScene().buildIndex + 1 < mexScene)
+        {
+            SceneLoader.instance.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            SceneLoader.instance.LoadScene("Menu");
+        }
+
         Time.timeScale = 1f;
     }
 }
